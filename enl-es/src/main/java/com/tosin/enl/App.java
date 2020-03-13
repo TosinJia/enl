220c089ca202a093cee93575156680540ea310b7
ts.jia <TosinJia@foxmail.com>
2020-03-13 21:05:06 +0800

    完善代码

---
+++ enl-es/src/main/java/com/tosin/enl/App.java
@@ -0,0 +1,13 @@
+package com.tosin.enl;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
