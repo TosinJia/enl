a266649e7fb7b2d747dcf786cfa4e2d6b23cef2c
ts.jia <TosinJia@foxmail.com>
2020-03-22 23:01:46 +0800

    构建springcloud项目

---
+++ enl_springcloud_dir/enl-springcloud-eureka/src/main/java/com/tosin/enl/springcloud/eureka/EurekaApplication.java
@@ -0,0 +1,14 @@
+package com.tosin.enl.springcloud.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//作为SpringBoot服务
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class})
+@EnableEurekaServer
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
---
+++ enl_springcloud_dir/enl-springcloud-hbase/src/main/java/com/tosin/enl/springcloud/hbase/controller/TestController.java
@@ -0,0 +1,35 @@
+package com.tosin.enl.springcloud.hbase.controller;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/test")
+@Api(value="测试 Api")
+public class TestController {
+
+    @ResponseBody
+    @RequestMapping(value = "/hello", method = {RequestMethod.GET, RequestMethod.POST})
+    public String hello(){
+        System.out.println("hello!");
+        return "hello!";
+    }
+
+    @ApiOperation(value = "测试信息 ApiOperation value",notes = "测试信息 ApiOperation notes")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "info",value = "信息 ApiImplicitParam value",required = true,dataType = "string")
+    })
+    @ResponseBody
+    @RequestMapping(value = "/hello-info", method = {RequestMethod.GET, RequestMethod.POST})
+    public String helloInfo(@RequestParam(name="info") String info){
+        System.out.println("hello "+info);
+        return "hello "+info;
+    }
+}
